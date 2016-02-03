c5d8d9f96d45127e521c4fe1ab3c03d204f89bc8
SimonCao <simon_cao@hotmail.com>
2016-02-03 20:29:33 +0800

    delete actionbar
    delete action bar, just normal activity

--- app/src/main/java/com/example/simoncao/widgettest/MainActivity.java
+++ app/src/main/java/com/example/simoncao/widgettest/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.simoncao.widgettest;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     Button button1, button2;
     EditText et;
@@ -22,6 +20,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         button1=(Button)findViewById(R.id.upload);
         button2=(Button)findViewById(R.id.download);
@@ -40,33 +39,13 @@ public class MainActivity extends ActionBarActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences share=getSharedPreferences("data",MODE_PRIVATE);
+                SharedPreferences share = getSharedPreferences("data", MODE_PRIVATE);
                 day = share.getString("Time", "defaultname");
-                Log.d("Tag","The Birthday is"+day);
+                Log.d("Tag", "The Birthday is" + day);
             }
         });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
